04524186d5af7444cdd846c9eb030452014ca217
Suryaprakash Maddheshiya <146697128+Suryaprakash-maddheshiya@users.noreply.github.com>
2024-02-09 00:04:51 +0530

    Create returnLastChar.js

---
+++ returnLastChar.js
@@ -0,0 +1,9 @@
+function getLastCharacter(name) {
+return name.at(-2);
+
+}
+
+// Sample usage - do not modify
+console.log(getLastCharacter("Sam")); // "m"
+console.log(getLastCharacter("Alex")); // "x"
+console.log(getLastCharacter("Charley")); // "y"
